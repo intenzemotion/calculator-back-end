746aa4322edd7e22a885e014196dd3649955d4e3
Ikram <wsuaimi@gmail.com>
2023-12-29 18:04:56 +0800

    Put allowed origins in config file, can use multiple origins

--- src/main/java/com/woodwing/calculator/CalculatorApplication.java
+++ src/main/java/com/woodwing/calculator/CalculatorApplication.java
@@ -1,5 +1,6 @@
 package com.woodwing.calculator;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,9 @@ import java.util.Arrays;
 @SpringBootApplication
 public class CalculatorApplication {
 
+    @Value("${cors.allowed-origins}")
+    private String allowedOrigins;
+
     public static void main(String[] args) {
         SpringApplication.run(CalculatorApplication.class, args);
     }
@@ -21,7 +25,7 @@ public class CalculatorApplication {
     public CorsFilter corsFilter() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowCredentials(true);
-        corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
         corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type", "Accept", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request-Method", "Access-Control-Request-Headers"));
         corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
