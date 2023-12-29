2f3c0ad84b1d1f11ea74a4df430121135a36c8d2
Ikram <wsuaimi@gmail.com>
2023-12-29 18:00:04 +0800

    Complete calculator service interface

--- src/main/java/com/woodwing/calculator/service/CalculatorService.java
+++ src/main/java/com/woodwing/calculator/service/CalculatorService.java
@@ -1,3 +1,12 @@
 package com.woodwing.calculator.service;
+
 public interface CalculatorService {
+
+    double add(double value1, String unit1, double value2, String unit2);
+
+    double subtract(double value1, String unit1, double value2, String unit2);
+
+    double multiply(double value1, String unit1, double value2, String unit2);
+
+    double divide(double value1, String unit1, double value2, String unit2);
 }
